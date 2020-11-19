9184308504a7e5f8ea8dd1ab71227775ba71a8eb
Ikille4fun <Alexander.wh@hotmail.com>
2020-11-19 15:25:48 +0100

    updated .xml files

--- Carlog/app/src/main/java/com/App_Project_Group_13/carlog/Activities/RegisteredVehicles.java
+++ Carlog/app/src/main/java/com/App_Project_Group_13/carlog/Activities/RegisteredVehicles.java
@@ -1,16 +1,56 @@
 package com.App_Project_Group_13.carlog.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.App_Project_Group_13.carlog.R;
 
 public class RegisteredVehicles extends AppCompatActivity {
 
+    //widgets
+    private TextView txtVehicleName;
+    private RecyclerView rcvList;
+    //private Adapter = adaptor;
+    private Button btnBack;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //init ui
+        setupUI();
+
+    }
+
+    private void setupUI() {
         setContentView(R.layout.activity_registered_vehicles);
+
+        //set up recyclerview with adapter and layout manager
+        //adapter = new (this); //skal oprates
+        rcvList = findViewById(R.id.rcvRegisteredVehicles);
+        rcvList.setLayoutManager(new LinearLayoutManager(this));
+        //rcvList.setAdapter(adapter); //skal oprates
+
+        txtVehicleName = findViewById(R.id.txtVehicleName);
+
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Back();
+            }
+        });
+    }
+
+    private void Back() {
+        finish();
     }
 }
--- Carlog/app/src/main/java/com/App_Project_Group_13/carlog/Activities/YourVehicles.java
+++ Carlog/app/src/main/java/com/App_Project_Group_13/carlog/Activities/YourVehicles.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.App_Project_Group_13.carlog.R;
 
-public class YourVehicle extends AppCompatActivity {
+public class YourVehicles extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_your_vehicle);
+        setContentView(R.layout.activity_your_vehicles);
     }
 }

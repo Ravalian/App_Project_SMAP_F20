e60e7bf72fc63fbb251fec663a4384c1455b3a7f
Ikille4fun <Alexander.wh@hotmail.com>
2020-11-19 13:36:25 +0100

    New project

---
+++ Carlog/app/src/main/java/com/App_Project_Group_13/carlog/MainActivity.java
@@ -0,0 +1,14 @@
+package com.App_Project_Group_13.carlog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
